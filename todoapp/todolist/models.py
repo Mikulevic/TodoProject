a7a3be9d4d56f489270177aa233b96209f498d24
Daniel <danielmikulevic@gmail.com>
2023-02-20 10:56:43 +0300

    prisijungimas

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
     name = models.CharField(max_length=200, null=True)
@@ -7,5 +8,3 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
-
-
--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
 
 
+
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter
     return render(request, 'todo/task_list.html', {'tasks': tasks})
+
 
 
 def task_detail(request, pk):
@@ -13,15 +16,19 @@ def task_detail(request, pk):
     return render(request, 'todo/task_detail.html', {'task': task})
 
 
+
 def task_create(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = form.save()
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
             return redirect('task_detail', pk=task.pk)
     else:
         form = TaskForm()
     return render(request, 'todo/task_form.html', {'form': form})
+
 
 
 def task_edit(request, pk):
@@ -34,6 +41,7 @@ def task_edit(request, pk):
     else:
         form = TaskForm(instance=task)
     return render(request, 'todo/task_form.html', {'form': form})
+
 
 
 def task_delete(request, pk):
